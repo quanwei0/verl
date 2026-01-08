536bfde15b8cbf25d70972eaff4fd71be8507318
quanwei0 <weiquan0128@gmail.com>
2026-01-08 22:09:17 +0000

    update code and data

--- examples/opd_trainer/README.md
+++ examples/opd_trainer/README.md
@@ -8,11 +8,19 @@ conda create -n verl-vllm python=3.12 -y
 pip install vllm==0.11.0
 pip install -e ".[vllm]"
 pip install flash-attn --no-build-isolation
+
+pip install polars
+```
+
+## Prepare Datasets
+
+```bash
+bash examples/opd_trainer/prepare_dapo_data.sh
 ```
 
 ## Run OPD Trainer
 
-distil qwen3 1.7b from qwen3 32b
+distil qwen3 4b from qwen3 32b
 ```bash
-bash examples/opd_trainer/run_qwen3-1.7b_opd.sh
+bash examples/opd_trainer/run_qwen3-4b_opd.sh
 ```
--- examples/opd_trainer/run_qwen3-4b_opd.sh
+++ examples/opd_trainer/run_qwen3-4b_opd.sh
@@ -4,43 +4,50 @@
 set -x
 
 
+
+DATA_DIR="$HOME/data/dapo"
+TRAIN_FILE="$DATA_DIR/dapo-math-17k-cleaned.parquet"
+TEST_FILE="$DATA_DIR/aime-2024-cleaned.parquet"
+TRAIN_BATCH_SIZE=256
+
+
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=opd \
-    data.train_files=$HOME/data/gsm8k/train.parquet \
-    data.val_files=$HOME/data/gsm8k/test.parquet \
-    data.train_batch_size=1024 \
-    data.max_prompt_length=512 \
-    data.max_response_length=1024 \
+    data.train_files=$TRAIN_FILE \
+    data.val_files=$TEST_FILE \
+    data.train_batch_size=$TRAIN_BATCH_SIZE \
+    data.max_prompt_length=2048 \
+    data.max_response_length=8192 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
-    actor_rollout_ref.model.path=Qwen/Qwen3-1.7B \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.path=Qwen/Qwen3-4B \
+    actor_rollout_ref.actor.optim.lr=1e-5 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=$TRAIN_BATCH_SIZE \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_kl_loss=False \
-    actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=1 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.ref.model.path=Qwen/Qwen3-32B \
     algorithm.use_kl_in_reward=True \
     algorithm.kl_ctrl.kl_coef=1 \
+    reward_model.reward_manager=dapo \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
-    trainer.project_name='verl_opd_example_gsm8k' \
-    trainer.experiment_name='qwen3_1.7b_8b_opd' \
-    trainer.n_gpus_per_node=4 \
+    trainer.project_name='verl_opd_dapo_math' \
+    trainer.experiment_name='qwen3_4b_32b_opd' \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
-    trainer.test_freq=-1 \
-    trainer.total_epochs=15 $@
+    trainer.test_freq=10 \
+    trainer.total_epochs=20 $@
